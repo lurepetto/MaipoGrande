1500f5d6b028ad9f1c08c34883bed9c983d3d594
lurepetto <lu.repetto@alumnos.duoc.cl>
2020-09-10 00:14:55 -0300

    Initial Commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../App.css'
+import { Button } from './Button'
+import './HeroSection.css'
+ 
+ function HeroSection() {
+     return (
+       <div className="hero-container">
+         <video src="/videos/video-1.mp4" autoPlay loop muted />
+         <h1>Feria Virtual</h1>
+         <div className="hero-btns">
+           <Button
+             className="btns"
+             buttonStyle="btn--outline"
+             buttonSize="btn--large"
+           >
+             Ingresa a tu perfil
+           </Button>
+           <Button
+             className="btns"
+             buttonStyle="btn--primary"
+             buttonSize="btn--large"
+           >
+             Contactanos
+           </Button>
+         </div>
+       </div>
+     );
+ }
+ 
+ export default HeroSection
+ 
