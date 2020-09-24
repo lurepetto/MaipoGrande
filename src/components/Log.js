997f74ca176d864edec3ee0318a227927ed9a40d
carenasc <cayoarenas14@gmail.com>
2020-09-24 20:24:32 -0300

    Validate Login Form

--- src/components/Log.js
+++ src/components/Log.js
@@ -2,6 +2,9 @@ import React, { Component } from "react"
 import sha1 from "sha1"
 import "../styles/Login.css"
 import { setObject } from '../utils/storage'
+import ValidateLogForm from './ValidateLogForm'
+
+
 
 class Log extends Component {
   state = {
@@ -9,6 +12,8 @@ class Log extends Component {
       username: "",
       password: "",
     },
+    errors: {
+    }
   };
 
   /* Método para capturar inputs */
@@ -21,6 +26,22 @@ class Log extends Component {
     });
     console.log(this.state.form); /*Imprimimos los datos ingresados en el estado en la consola */
 }
+
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const { errors, ...sinErrors } = this.state.form
+    const result = ValidateLogForm.validate(sinErrors)
+    
+    this.setState({ errors: result })
+    if(Object.keys(result).length === 0) {
+      // Enviar el formulario!
+      this.iniciarSesion()
+    }
+    
+  }
+
+
 
   /* Método para hacer la petición http */
   iniciarSesion = () => {
@@ -36,18 +57,12 @@ class Log extends Component {
       })
         .then(response =>  response.json())
         .then(data => {
-<<<<<<< HEAD
-          console.log(data.ProfileName)
-          if (data.ProfileName === "Productor") {
-            window.location.href = "/Productor";
-=======
           const rol = data.ProfileName; /*Creación de una variable con el rol del usuario */
           console.log(rol)
           setObject('profileName', rol )
           /* Ciclo para rutear a página según rol */
           if (rol === "Productor") {
             return window.open("/productor", "_self");
->>>>>>> bc6dd9279b5fa7255262c270f7c8c162b58f01b6
           }
           else if (rol === "Cliente") {
             return window.open("/cliente", "_self");
@@ -60,38 +75,45 @@ class Log extends Component {
         .catch((error) => alert("Error detected: " + error));
     }
 
+
+
   render() {
+    const { errors } = this.state
     return (
-      <div className="containerPrincipal">
-        <div className="containerSecundario">
-          <div className="form-group">
-            <label>Usuario: </label>
-            <br />
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              onChange={this.handleChange}
-            />
-            <br />
-            <label>Contraseña: </label>
-            <br />
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              onChange={this.handleChange}
-            />
-            <br />
-            <button
-              className="btn btn-primary"
-              onClick={() => this.iniciarSesion()}
-            >
-              Iniciar Sesión
-            </button>
+      <form onSubmit={this.handleSubmit}>
+        <div className="containerPrincipal">
+          <div className="containerSecundario">
+            <div className="form-group">
+              <label>Usuario: </label>
+              <br />
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                onChange={this.handleChange}
+              />
+              {errors.username && <p>{ errors.username }</p>}
+              <br />
+              <label>Contraseña: </label>
+              <br />
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                onChange={this.handleChange}
+              />
+              {errors.password && <p>{ errors.password }</p>}
+              <br />
+              <button
+                className="btn btn-primary"
+                // onClick={() => this.iniciarSesion()}
+              >
+                Iniciar Sesión
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+     </form> 
     );
   }
 }
